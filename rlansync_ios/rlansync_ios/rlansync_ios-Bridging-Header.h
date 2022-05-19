3ccb6c45cd44fcd3164cfc71bd1b41b7b8f34e84
stoprain <mystoprain@gmail.com>
2022-05-19 22:50:26 +0800

    callback for swift

--- rlansync_ios/rlansync_ios/rlansync_ios-Bridging-Header.h
+++ rlansync_ios/rlansync_ios/rlansync_ios-Bridging-Header.h
@@ -2,4 +2,18 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <Foundation/Foundation.h>
+
 int shipping_rust_addition(int a, int b);
+
+typedef struct CompletedCallback {
+    void * _Nonnull userdata;
+    void (* _Nonnull callback)(void * _Nonnull, const char*);
+} CompletedCallback;
+
+void notify(const char* from, CompletedCallback callback);
+
+#endif
---
+++ rlansync_ios/rlansync_ios/rlansynclib.swift
@@ -0,0 +1,30 @@
+//
+//  rlansynclib.swift
+//  rlansync_ios
+//
+//  Created by Rain Qian on 2022/5/19.
+//
+
+import Foundation
+import YNLib
+
+private class WrapClosure<T> {
+    fileprivate let closure: T
+    init(closure: T) {
+        self.closure = closure
+    }
+}
+
+public func RAsyncOperation(closure: @escaping (UnsafePointer<Int8>) -> Void) {
+    let wrappedClosure = WrapClosure(closure: closure)
+    let userdata = Unmanaged.passRetained(wrappedClosure).toOpaque()
+
+    let callback: @convention(c) (UnsafeMutableRawPointer, UnsafePointer<Int8>?) -> Void = { (_ userdata: UnsafeMutableRawPointer, _ success: UnsafePointer<Int8>?) in
+        let wrappedClosure: WrapClosure<(UnsafePointer<Int8>?) -> Void> = Unmanaged.fromOpaque(userdata).takeRetainedValue()
+        wrappedClosure.closure(success)
+    }
+
+    let completion = CompletedCallback(userdata: userdata, callback: callback)
+    print("documentPath \(AppSandboxHelper.documentsPath)")
+    notify(AppSandboxHelper.documentsPath.cString(using: .utf8)!, completion)
+}
