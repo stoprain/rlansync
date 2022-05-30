34bb8ee7ccc1b326f3b7df86a2a3c05903309c4c
stoprain <mystoprain@gmail.com>
2022-05-30 10:09:30 +0800

    share extension

--- rlansync_ios/rlansync_ios/ContentView.swift
+++ rlansync_ios/rlansync_ios/ContentView.swift
@@ -9,16 +9,80 @@ import SwiftUI
 import YNLib
 
 struct ContentView: View {
+    
+    let sharedIdentifier = "group.com.stoprain.rlansync"
+    
+    @State private var items = [URL]()
+    @State private var toolbarLinkSelected = false
+    
     var body: some View {
-        Text("Hello, world! \(shipping_rust_addition(30, 1))")
-            .padding()
-            .onAppear {
-                DispatchQueue.global().async {
-                    RAsyncOperation { result in
-                        print("RAsyncOperation \(String(cString: result))")
+        NavigationView {
+            List {
+                ForEach(items, id: \.self) { item in
+                    NavigationLink {
+                        if item.absoluteString.contains(".txt") {
+                            ItemTextView(path: item.absoluteString)
+                        } else {
+                            Text(item.lastPathComponent)
+                        }
+                    } label: {
+                        Text(item.lastPathComponent)
                     }
                 }
             }
+            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    EditButton()
+//                }
+                ToolbarItem {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+            .background {
+                NavigationLink(destination: ItemTextView(path: ""),
+                               isActive: $toolbarLinkSelected) {
+                    
+                }.hidden()
+            }
+            Text("Detail pane")
+        }
+        .onAppear {
+            let fileManager = FileManager.default
+            let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            do {
+                items.removeAll()
+                let fileURLs = try fileManager.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil)
+                for url in fileURLs {
+//                    items.append(url.absoluteString)
+                    items.append(url)
+                }
+            } catch {
+                print("Error while enumerating files \(documentsURL.path): \(error.localizedDescription)")
+            }
+            
+            if let prefs = UserDefaults(suiteName: sharedIdentifier) {
+                let t = prefs.string(forKey: "share.public.plain-text")
+                print("##### \(t)")
+                
+                let tt = prefs.array(forKey: "share.public.image")
+                print("##### \(tt)")
+            }
+        }
+//        Text("Hello, world! \(shipping_rust_addition(30, 1))")
+//            .padding()
+//            .onAppear {
+////                DispatchQueue.global().async {
+////                    RAsyncOperation { result in
+////                        print("RAsyncOperation \(String(cString: result))")
+////                    }
+////                }
+//            }
+    }
+    
+    private func addItem() {
+        toolbarLinkSelected = true
     }
 }
 
---
+++ rlansync_ios/share/ShareViewController.swift
@@ -0,0 +1,171 @@
+//
+//  ShareViewController.swift
+//  share
+//
+//  Created by Rain Qian on 2022/5/29.
+//
+
+import UIKit
+import Social
+import UniformTypeIdentifiers
+
+/*
+ https://developer.apple.com/design/human-interface-guidelines/ios/extensions/sharing-and-actions/
+ https://dmtopolog.com/ios-app-extensions-data-sharing/
+ 
+ https://developer.apple.com/documentation/uniformtypeidentifiers/uttype
+ https://developer.apple.com/documentation/uniformtypeidentifiers/system_declared_uniform_type_identifiers
+ https://daringfireball.net/linked/2011/08/05/markdown-uti
+ https://www.jianshu.com/p/c6b34eb5f753
+ */
+
+class ShareViewController: SLComposeServiceViewController {
+    
+    let sharedIdentifier = "group.com.stoprain.rlansync"
+
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
+
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+//        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        
+//        print("#################")
+        
+//        print(extensionContext?.inputItems)
+        
+        /*
+         >> Chrome
+         Optional([<NSExtensionItem: 0x280b94e40> - userInfo: {
+             NSExtensionItemAttachmentsKey =     (
+                 "<NSItemProvider: 0x28229a760> {types = (\n    \"public.url\"\n)}"
+             );
+             "com.apple.UIKit.NSExtensionItemUserInfoIsContentManagedKey" = 0;
+         }])
+         
+         >> Photos
+         ### image Optional(file:///var/mobile/Media/PhotoData/OutgoingTemp/[UUID]/IMG_4823.jpg)
+         ### image Optional(file:///var/mobile/Media/PhotoData/OutgoingTemp/[UUID]/IMG_4824.jpg)
+         */
+        
+        /*
+         
+         Transfer format
+         
+         uuid.public.plain-text
+             # [contentText]
+             [public.plain-text]
+             [public.url]
+         
+         uuid.public.image
+            []
+         
+         */
+        print("#################")
+        var text = "#\(contentText ?? "")\n"
+        var images = [Data]()
+        if let prefs = UserDefaults(suiteName: sharedIdentifier) {
+            if let item = extensionContext?.inputItems.first as? NSExtensionItem {
+                for itemProvider in item.attachments ?? [] {
+                    if itemProvider.hasItemConformingToTypeIdentifier("public.plain-text") {
+                        itemProvider.loadItem(forTypeIdentifier: "public.plain-text", options: nil) { data, error in
+                            if let t = data as? String {
+                                text += t + "\n"
+                                prefs.set(text, forKey: "share.public.plain-text")
+                            }
+//                            print("## plain-text \(data)")
+                        }
+                    }
+                    if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+                        itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil) { data, error in
+                            if let t = data as? String {
+                                text += t + "\n"
+                                prefs.set(text, forKey: "share.public.plain-text")
+                            }
+//                            print("## url \(data)")
+                        }
+                    }
+                    if itemProvider.hasItemConformingToTypeIdentifier("public.image") {
+//                        itemProvider.loadItem(forTypeIdentifier: "public.image", options: nil) { data, error in
+//                            if let t = data as? UIImage {
+////                                values["public.image"]
+////                                text += t + "\n"
+//                            }
+//                            print("### image \(data)")
+//                        }
+                        
+                        itemProvider.loadObject(ofClass: UIImage.self) { data, error in
+                            images.append((data as? UIImage)?.pngData() as! Data)
+                            prefs.set(images, forKey: "share.public.image")
+//                            print("### image \(data)")
+                        }
+                    }
+                }
+            }
+            print("### \(text)")
+        }
+        
+        
+//            print("attributedContentText \(item.attributedContentText)")
+//            print("attributedTitle \(item.attributedTitle)")
+//            print("userinfo \(item.userInfo?["NSExtensionItemAttributedContentTextKey"])")
+//            for i in item.attachments ?? [] {
+//                if i.hasItemConformingToTypeIdentifier("public.image") {
+//                    i.loadItem(forTypeIdentifier: "public.image", options: nil) { data, error in
+//                        print("## image \(data)")
+//                    }
+//                }
+//                if i.hasItemConformingToTypeIdentifier("public.plain-text") {
+//                    i.loadItem(forTypeIdentifier: "public.plain-text", options: nil) { data, error in
+//                        print("## plain-text \(data)")
+//                    }
+//                }
+//                if i.hasItemConformingToTypeIdentifier("public.url") {
+//                    i.loadItem(forTypeIdentifier: "public.url", options: nil) { data, error in
+//                        print("## url \(data)")
+//                    }
+//                }
+////                if i.canLoadObject(ofClass: String.self) {
+////                    i.loadObject(ofClass: String.self) { data, error in
+////                        print("##### string \(data)")
+////                    }
+////                } else if i.canLoadObject(ofClass: UIImage.self) {
+////                    i.loadObject(ofClass: UIImage.self) { data, error in
+////                        print("##### image \(data)")
+////                    }
+////                }
+//            }
+//           let itemProvider = item.attachments?.first as? NSItemProvider,
+//           itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+//            itemProvider.loadItem(forTypeIdentifier: "public.url") { url, error in
+//                if let shareURL = url as? URL {
+//                    print(shareURL)
+//                }
+//            }
+        
+        
+//        print(textView.text)
+//        print(contentText)
+        
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
+
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        
+//        if let prefs = UserDefaults(suiteName: sharedIdentifier) {
+//            let t = prefs.data(forKey: "share.public.plain-text")
+//            print("##### \(t)")
+//            
+//            let tt = prefs.data(forKey: "share.public.image")
+//            print("##### \(tt)")
+//        }
+        
+        return []
+    }
+
+}
