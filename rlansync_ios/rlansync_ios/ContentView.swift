30561d7aded91f38441fdec1d01333a48e6ed2b4
stoprain <mystoprain@gmail.com>
2022-05-31 10:06:07 +0800

    order by date

--- rlansync_ios/rlansync_ios/ContentView.swift
+++ rlansync_ios/rlansync_ios/ContentView.swift
@@ -12,21 +12,22 @@ struct ContentView: View {
     
     let sharedIdentifier = "group.com.stoprain.rlansync"
     
-    @State private var items = [URL]()
+    @State private var items = [SimpleShareItem]()
     @State private var toolbarLinkSelected = false
+    @ObservedObject var observer = Observer()
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(items, id: \.self) { item in
+                ForEach(items, id: \.url) { item in
                     NavigationLink {
-                        if item.absoluteString.contains(".txt") {
-                            ItemTextView(path: item.absoluteString)
+                        if item.url.absoluteString.contains(".txt") {
+                            ItemTextView(path: item.url.absoluteString)
                         } else {
-                            ItemDirectoryView(path: item.absoluteString)
+                            ItemDirectoryView(path: item.url.absoluteString)
                         }
                     } label: {
-                        Text(item.lastPathComponent)
+                        Text("\(item.url.lastPathComponent) - \(item.date, formatter: itemFormatter)")
                     }
                 }
             }
@@ -49,45 +50,12 @@ struct ContentView: View {
             Text("Detail pane")
         }
         .onAppear {
-            let fileManager = FileManager.default
-            let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
-            do {
-                items.removeAll()
-                let fileURLs = try fileManager.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil)
-                for url in fileURLs {
-//                    items.append(url.absoluteString)
-                    items.append(url)
-                }
-            } catch {
-                print("Error while enumerating files \(documentsURL.path): \(error.localizedDescription)")
-            }
-            
-            if let prefs = UserDefaults(suiteName: sharedIdentifier) {
-                let uuids = prefs.array(forKey: "share.uuids") as? [String]
-                let allkeys = prefs.dictionaryRepresentation().keys
-                for uuid in uuids ?? [] {
-                    print("###### \(uuid)")
-                    var text = ""
-                    var images = [Data]()
-                    for key in allkeys {
-                        if key.contains(uuid) {
-                            if key.contains("public.plain-text") {
-                                text = prefs.string(forKey: key) ?? ""
-                            } else if key.contains("public.image") {
-                                let imageDatas = prefs.array(forKey: key) as? [Data]
-                                for data in imageDatas ?? [] {
-                                    images.append(data)
-                                }
-                            }
-                        }
-                    }
-                    let si = ShareItem(uuid: uuid, text: text, images: images)
-                    si.save()
-                }
-                for key in allkeys {
-                    prefs.removeObject(forKey: key)
-                }
-                prefs.removeObject(forKey: "share.uuids")
+            let _ = loadFromSuite()
+            loadFromDocument()
+        }
+        .onReceive(observer.$enteredForeground) { _ in
+            if loadFromSuite() {
+                loadFromDocument()
             }
         }
 //        Text("Hello, world! \(shipping_rust_addition(30, 1))")
@@ -101,8 +69,90 @@ struct ContentView: View {
 //            }
     }
     
+    private func loadFromDocument() {
+        let fileManager = FileManager.default
+        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        do {
+            items.removeAll()
+            let fileURLs = try fileManager.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil)
+            for url in fileURLs {
+                let attr = try? fileManager.attributesOfItem(atPath: url.path)
+                if let date = attr?[FileAttributeKey.modificationDate] as? Date {
+                    let item = SimpleShareItem(url: url, date: date)
+                    items.append(item)
+                }
+            }
+            items.sort {
+                $0.date > $1.date
+            }
+        } catch {
+            print("Error while enumerating files \(documentsURL.path): \(error.localizedDescription)")
+        }
+    }
+    
+    private func loadFromSuite() -> Bool {
+        var hasChange = false
+        if let prefs = UserDefaults(suiteName: sharedIdentifier) {
+            let uuids = prefs.array(forKey: "share.uuids") as? [String]
+            let allkeys = prefs.dictionaryRepresentation().keys
+            for uuid in uuids ?? [] {
+                print("###### \(uuid)")
+                hasChange = true
+                var text = ""
+                var images = [Data]()
+                for key in allkeys {
+                    if key.contains(uuid) {
+                        if key.contains("public.plain-text") {
+                            text = prefs.string(forKey: key) ?? ""
+                        } else if key.contains("public.image") {
+                            let imageDatas = prefs.array(forKey: key) as? [Data]
+                            for data in imageDatas ?? [] {
+                                images.append(data)
+                            }
+                        }
+                    }
+                }
+                let si = ShareItem(uuid: uuid, text: text, images: images)
+                si.save()
+            }
+            for key in allkeys {
+                prefs.removeObject(forKey: key)
+            }
+            prefs.removeObject(forKey: "share.uuids")
+        }
+        return hasChange
+    }
+    
     private func addItem() {
         toolbarLinkSelected = true
+    }
+    
+    private let itemFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+}
+
+class Observer: ObservableObject {
+
+    @Published var enteredForeground = true
+
+    init() {
+        if #available(iOS 13.0, *) {
+            NotificationCenter.default.addObserver(self, selector: #selector(willEnterForeground), name: UIScene.willEnterForegroundNotification, object: nil)
+        } else {
+            NotificationCenter.default.addObserver(self, selector: #selector(willEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+        }
+    }
+
+    @objc func willEnterForeground() {
+        enteredForeground.toggle()
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
 
