c7bb8e4ab4cd2ded7b09b6ad5e8c5fdc61e469cd
stoprain <mystoprain@gmail.com>
2022-06-14 23:13:47 +0800

    in progress

---
+++ rlansync_lib/src/utils.rs
@@ -0,0 +1,19 @@
+use std::io;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+pub fn write_head_and_bytes(mut stream: &TcpStream, data: &[u8]) -> io::Result<()> {
+    let buffer = (data.len() as u32).to_be_bytes();
+    stream.write_all(&buffer)?;
+    stream.write_all(data)?;
+    Ok(())
+}
+
+pub fn read_head_and_bytes(mut stream: &TcpStream) -> io::Result<Vec<u8>> {
+    let mut buffer = [0u8; 4];
+    stream.read_exact(&mut buffer[..])?;
+    let size = u32::from_be_bytes(buffer);
+    let mut payload = vec![0; size as usize];
+    stream.read_exact(&mut payload[..])?;
+    Ok(payload)
+}
